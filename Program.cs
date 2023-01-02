4c48abf215dd9704b7693e69477d2260498fffbc
Abel Almada <abel.almada.1992@gmail.com>
2023-01-01 22:49:12 -0600

    Numeros complejos commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace source
+{
+    class Program
+    {
+        public struct NComplejo
+        {
+            public int real;
+            public int imaginario;
+        }
+
+        NComplejo sumaComplejos(NComplejo a, NComplejo b)
+        {
+            NComplejo resultado;
+            resultado.real = a.real + b.real;
+            resultado.imaginario = a.imaginario + b.imaginario;
+
+            return resultado;
+        }
+
+        string ComplejoToString(NComplejo a)
+        {
+            string s;
+            s = a.real.ToString();
+            s += (a.imaginario >= 0 ? " + " : "");
+            s += a.imaginario.ToString()+"i";
+
+            return s;
+        }
+
+        void OperacionesConComplejos()
+        {
+            var a = new NComplejo {real = 4, imaginario = 10};
+            var b = new NComplejo {real = 4, imaginario = 10};
+
+            var resul = sumaComplejos(a, b);
+
+            Console.WriteLine(" ( " + ComplejoToString(a) + " ) " + " +" +" ( " + ComplejoToString(b) + " ) " + " = " + ComplejoToString(resul));
+        }
+
+        static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.OperacionesConComplejos();
+        }
+    }
+}
